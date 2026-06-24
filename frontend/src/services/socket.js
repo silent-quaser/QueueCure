66ab4b32df23e22bcf2bcfb915d789be00a97612
silent-quasar <naveenkrishnan1978@gmail.com>
2026-06-24 17:24:32 +0530

    Fix production socket connection

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  "http://localhost:5000";
+  "https://queuecure-backend-j3cg.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: true,
